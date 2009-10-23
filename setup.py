e1e7c375ffaa55dadffd9b66a3be37c3e1fcb655
Corey Oordt <coordt@washingtontimes.com>
2009-10-23 10:59:06 -0400

    Added content to the setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='django-googledata',
+      version='0.1',
+      description='A Django app to interface with google data.',
+      long_description='',
+      author='Corey Oordt, The Washington Times',
+      author_email='coordt@washingtontimes.com',
+      url='http://opensource.washingtontimes.com/projects/django-googledata/',
+      packages=['googledata'],
+      classifiers=[],
+)
